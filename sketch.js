6baba5f7becd62e0b32498281ba17019c1f5f6b2
SanthyaVignesh <74100644+SanthyaVignesh@users.noreply.github.com>
2020-12-23 22:29:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10;
-var box11,box12,box13,box14,box15,box17,box18,box19,box20;
+//var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10;
+//var box11,box12,box13,box14,box15,box17,box18,box19,box20;
+var boxes1 = [],boxes2=[],boxes3=[];
 var ground,ball,rope,score=0;
 var engine,world,bgimg;
 
@@ -15,31 +16,22 @@ function preload(){
 function setup() {
   createCanvas(1500,600);
 
-  engine = Engine.create();
+  engine = Engine. create();
   world = engine.world;
 
   ground = new Ground(500,570,1000,10);
 
-  box1 =  new Blocks(800,550);
-  box2 = new Blocks(800,480);
-  box3 = new Blocks(800,400);
-  box4 = new Blocks(800,330);
-  box5 = new Blocks(800,280);
-  box6 = new Blocks(800,200);
-  box7 = new Blocks(800,130);
-  box8 = new Blocks(800,80);
-  box9 =  new Blocks(730,550);
-  box10 = new Blocks(730,500);
-  box11 = new Blocks(730,450);
-  box12 = new Blocks(730,400);
-  box13 = new Blocks(730,350);
-  box14 = new Blocks(660,550);
-  box15 = new Blocks(660,500);
-  box16 = new Blocks(660,450);
-  box17 = new Blocks(660,400);
-  box18 = new Blocks(660,350);
-  box19 = new Blocks(660,300);
-  box20 = new Blocks(660,250);
+  for(var i=0; i<=7; i++){
+    boxes1[i] = new Blocks(700,100+i*70,70,70);
+  }
+
+  for(var i=0; i<=7; i++){
+    boxes2[i] = new Blocks(800,100+i*70,70,70);
+  }
+
+  for(var i=0; i<=7; i++){
+    boxes3[i] = new Blocks(900,100+i*70,70,70);
+  }
 
   ball = new Ball(500,80);
 
@@ -56,28 +48,18 @@ function draw() {
   fill("Magenta");
   text("Score  : " +score,950,100);
   pop();
-  
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display();
-  box5.display();
-  box6.display();
-  box7.display();
-  box8.display();
-  box9.display();
-  box10.display();
-  box11.display();
-  box12.display();
-  box13.display();
-  box14.display();
-  box15.display();
-  box16.display();
-  box17.display();
-  box18.display();
-  box19.display();
-  box20.display();
+  for(var i = 0; i < boxes1.length;i++){
+    boxes1[i].display();
+  }
 
+
+  for(var i = 0; i < boxes2.length;i++){
+    boxes2[i].display();
+  }
+
+  for(var i = 0; i < boxes3.length;i++){
+    boxes3[i].display();
+  }
   ball.display();
   rope.display();
 
